631bc2a5f0c1b89285823dc7894ec10239814a07
Anton Lokhmotov <psyhtest@users.noreply.github.com>
2020-11-27 16:13:19 +0000

    Revert "optimized image loading (use numpy arrays instead of a dictionary)"
    This reverts commit 6e0a9576867461522845e4451b79845f5a82dded.

--- program/image-classification-tensorrt-loadgen-py/tensorrt_classify_loadgen.py
+++ program/image-classification-tensorrt-loadgen-py/tensorrt_classify_loadgen.py
@@ -7,8 +7,7 @@ import sys
 import time
 
 from imagenet_helper import (load_image_by_index_and_normalize, image_list, class_labels,
-    MODEL_DATA_LAYOUT, MODEL_USE_DLA, BATCH_SIZE,
-    MODEL_IMAGE_CHANNELS, MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, MODEL_INPUT_DATA_TYPE)
+    MODEL_DATA_LAYOUT, MODEL_USE_DLA, BATCH_SIZE)
 
 from tensorrt_helper import (initialize_predictor, inference_for_given_batch)
 
@@ -55,9 +54,8 @@ def tick(letter, quantity=1):
         print(letter + (str(quantity) if quantity>1 else ''), end='')
 
 
-# Currently loaded preprocessed images are stored in pre-allocated numpy arrays:
-preprocessed_image_buffer = np.empty((LOADGEN_BUFFER_SIZE, MODEL_IMAGE_CHANNELS, MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH), dtype=MODEL_INPUT_DATA_TYPE)
-preprocessed_image_map = np.empty(LOADGEN_DATASET_SIZE, dtype=np.int)   # this type should be able to hold indices in range 0:LOADGEN_DATASET_SIZE
+# Currently loaded preprocessed images are stored in a dictionary:
+preprocessed_image_buffer = {}
 
 
 def load_query_samples(sample_indices):     # 0-based indices in our whole dataset
@@ -66,10 +64,10 @@ def load_query_samples(sample_indices):     # 0-based indices in our whole datas
 
     tick('B', len(sample_indices))
 
-    for buffer_index, sample_index in zip(range(len(sample_indices)), sample_indices):
-        preprocessed_image_map[sample_index] = buffer_index
-        preprocessed_image_buffer[buffer_index] = np.array( load_image_by_index_and_normalize(sample_index) )
+    for sample_index in sample_indices:
+        img = load_image_by_index_and_normalize(sample_index)
 
+        preprocessed_image_buffer[sample_index] = np.array(img)
         tick('l')
 
     if VERBOSITY_LEVEL:
@@ -78,6 +76,9 @@ def load_query_samples(sample_indices):     # 0-based indices in our whole datas
 
 def unload_query_samples(sample_indices):
     #print("unload_query_samples({})".format(sample_indices))
+    global preprocessed_image_buffer
+
+    preprocessed_image_buffer = {}
     tick('U')
 
     if VERBOSITY_LEVEL:
@@ -97,7 +98,7 @@ def issue_queries(query_samples):
 
     for j in range(0, len(query_samples), BATCH_SIZE):
         batch       = query_samples[j:j+BATCH_SIZE]   # NB: the last one may be shorter than BATCH_SIZE in length
-        batch_data  = preprocessed_image_buffer[preprocessed_image_map[ [qs.index for qs in batch] ]]
+        batch_data  = [ preprocessed_image_buffer[qs.index] for qs in batch ]
 
         trimmed_batch_results, inference_time_s = inference_for_given_batch(batch_data)
         actual_batch_size = len(trimmed_batch_results)
