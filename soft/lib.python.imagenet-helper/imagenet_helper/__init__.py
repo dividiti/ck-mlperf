4b3d6e2ffaedaa159ba0d55cd0e419cc56b5e7f3
Anton Lokhmotov <psyhtest@users.noreply.github.com>
2020-11-27 16:13:19 +0000

    Revert "bugfix: consider the shape of the first image when building the initial empty buffer"
    This reverts commit 3db3cadd6f898c2835a1c4c065b473bca17d318a.

--- soft/lib.python.imagenet-helper/imagenet_helper/__init__.py
+++ soft/lib.python.imagenet-helper/imagenet_helper/__init__.py
@@ -107,12 +107,9 @@ def load_image_by_index_and_normalize(image_index):
 
 
 def load_preprocessed_batch(image_list, image_index):
-    batch_data = None
+    batch_data = np.empty([BATCH_SIZE, MODEL_IMAGE_CHANNELS, MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH], dtype=MODEL_INPUT_DATA_TYPE)
     for in_batch_idx in range(BATCH_SIZE):
-        img = load_image_by_index_and_normalize(image_index)
-        if batch_data is None:
-            batch_data = np.empty( (BATCH_SIZE, *img.shape), dtype=MODEL_INPUT_DATA_TYPE)
-        batch_data[in_batch_idx] = img
+        batch_data[in_batch_idx] = load_image_by_index_and_normalize(image_index)
         image_index += 1
 
     #print('Data shape: {}'.format(batch_data.shape))
